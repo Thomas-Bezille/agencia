02ef4269bb766016bf60bc0eeb96005e683ad34f
thomas <bezille.thomasb@gmail.com>
2024-06-17 16:30:43 +0200

    📦 NEW: Dynamisation de la partie articles

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ require_once 'inc/supports.php';
 require_once 'inc/assets.php';
 require_once 'inc/apparence.php';
 require_once 'inc/menus.php';
+require_once 'inc/images.php';
 
 function agencia_icon(string $name): string
 {
